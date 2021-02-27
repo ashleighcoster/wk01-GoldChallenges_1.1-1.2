f40968b1dd2fb42c1aeeeb7291c3c3350d20c9a0
ashleighcoster <ashleigh.coster@gmail.com>
2021-02-26 22:36:13 -0500

    first try

--- 1.2-arrays-objects.instructions.js
+++ 1.2-arrays-objects.instructions.js
@@ -7,3 +7,31 @@
 */
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+let favoriteMovie = {
+    nameOfMovie: 'Love and Basketball',
+    runTime: "2hr 5m",
+    characters: [
+       {characterOne: 'Sanaa Lathan', 
+       age: 49, items: [
+           {herFirstMovie: 'Drive', 
+           herLastMovie: 'Native Son'},
+           {awardsWon: 7, 
+            from: 'NYC'}]},
+       {characterTwo: 'Omar Epps', 
+       age: 47, items: [
+           {hisFirstMovie: 'Juice', 
+           hisLastMovie: 'Fatal Affair'}, 
+           {awardsWon: 5, 
+            from: 'Brooklyn, NY'}]} 
+    ]
+}
+
+console.log(favoriteMovie.nameOfMovie);
+console.log(favoriteMovie.runTime);
+console.log(favoriteMovie.characters);
+console.log(favoriteMovie.characters[0].characterOne);
+console.log(favoriteMovie.characters[1].items[0].hisFirstMovie);
+
+
+
